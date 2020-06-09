74ad6ab0315fd3dd52f83530e0f9d6a872ded1e2
Greg Rynkowski <greg@twobuffers.com>
2020-06-09 13:19:48 +0200

    Make transition slower

--- app/src/main/java/com/twobuffers/circularviewpager2/EndlessScrolling.kt
+++ app/src/main/java/com/twobuffers/circularviewpager2/EndlessScrolling.kt
@@ -1,6 +1,11 @@
 package com.twobuffers.circularviewpager2
 
+import android.animation.Animator
+import android.animation.TimeInterpolator
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.coroutineScope
@@ -24,32 +29,61 @@ class EndlessScrollAdapter constructor(
 
     fun getCenterPage(position: Int = 0): Int {
         val middle = Integer.MAX_VALUE / 2
-        return middle - position.rem(items.size)  + 1 + position
+        return middle - position.rem(items.size) + 1 + position
     }
 }
 
 fun ViewPager2.setupEndlessScrollAdapter(
-    fm: FragmentManager,
-    lifeCycle: Lifecycle,
+    activity: FragmentActivity,
     list: List<Fragment>
-): FragmentStateAdapter {
-    val endlessAdapter = EndlessScrollAdapter(fm, lifeCycle, list)
+) {
+    val endlessAdapter = EndlessScrollAdapter(
+        activity.supportFragmentManager,
+        activity.lifecycle,
+        list
+    )
     adapter = endlessAdapter
     setCurrentItem(endlessAdapter.getCenterPage(), false)
-    return endlessAdapter
 }
 
 inline fun ViewPager2.setupEndlessScrollAdapter(
-    fm: FragmentManager,
-    lifeCycle: Lifecycle,
+    activity: FragmentActivity,
     block: () -> List<Fragment>
-) = setupEndlessScrollAdapter(fm, lifeCycle, block())
+) = setupEndlessScrollAdapter(activity, block())
 
 fun ViewPager2.setupAutomaticScroll(lifeCycle: Lifecycle, delayMillis: Long = 10_000) {
     lifeCycle.coroutineScope.launch {
         repeat(Int.MAX_VALUE) {
             delay(delayMillis)
-            currentItem += 1
+            setCurrentItem(currentItem + 1, duration = 1_100, pagePxToDrag = height)
         }
     }
+}
+
+// setCurrentItem with a slower animation
+// https://stackoverflow.com/questions/57505875/change-viewpager2-scroll-speed-when-sliding-programmatically/59235979#59235979
+fun ViewPager2.setCurrentItem(
+    item: Int,
+    duration: Long,
+    interpolator: TimeInterpolator = AccelerateDecelerateInterpolator(),
+    pagePxToDrag: Int = width // Default value taken from getWidth() from ViewPager2 view,
+) {
+    val pxToDrag: Int = pagePxToDrag * (item - currentItem)
+    val animator = ValueAnimator.ofInt(0, pxToDrag)
+    var previousValue = 0
+    animator.addUpdateListener { valueAnimator ->
+        val currentValue = valueAnimator.animatedValue as Int
+        val currentPxToDrag = (currentValue - previousValue).toFloat()
+        fakeDragBy(-currentPxToDrag)
+        previousValue = currentValue
+    }
+    animator.addListener(object : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator?) { beginFakeDrag() }
+        override fun onAnimationEnd(animation: Animator?) { endFakeDrag() }
+        override fun onAnimationCancel(animation: Animator?) { /* Ignored */ }
+        override fun onAnimationRepeat(animation: Animator?) { /* Ignored */ }
+    })
+    animator.interpolator = interpolator
+    animator.duration = duration
+    animator.start()
 }
--- app/src/main/java/com/twobuffers/circularviewpager2/MainActivity.kt
+++ app/src/main/java/com/twobuffers/circularviewpager2/MainActivity.kt
@@ -11,9 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewPager.setupEndlessScrollAdapter(supportFragmentManager, lifecycle) {
+        binding.viewPager.setupEndlessScrollAdapter(this) {
             listOf(Fragment1(), Fragment2(), Fragment3(), Fragment4())
         }
         binding.viewPager.setupAutomaticScroll(lifecycle)
+        // When uncommented, the viewpager can't be swiped by user.
+//        binding.viewPager.isUserInputEnabled = false
     }
 }
